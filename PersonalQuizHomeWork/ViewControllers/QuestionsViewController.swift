799d3b463f56a7bda3ef25a8dbea58735f585ef6
CemenVy <semen.vydrin0108@gmail.com>
2023-08-11 14:44:24 +0400

    Added outlets

--- PersonalQuizHomeWork/ViewControllers/QuestionsViewController.swift
+++ PersonalQuizHomeWork/ViewControllers/QuestionsViewController.swift
@@ -8,22 +8,26 @@
 import UIKit
 
 final class QuestionsViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabel: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
